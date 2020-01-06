44b51560ffa592a5225ecd09ca66ff38f9785c59
aya <aya.yoshida8@gmail.com>
2020-01-06 13:20:48 -0500

    one high score from local storage and timer subtracted for wrong question.

--- questions.js
+++ questions.js
@@ -1,18 +1,43 @@
 var questions = [ 
   {
     title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    choice1: "strings",
+    choice2: "booleans",
+    choice3: "alerts",
+    choice4: "numbers",
+    answer: 3
   },
   {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    title: "What is the HTML tag under which one can write the JavaScript code?",
+    choice1: "<javascript>",
+    choice2: "<scripted>",
+    choice3: "<script>",
+    choice4: "<js>",
+    answer: 3
   },
   {
-    title: "",
-    choices: "",
-    answer: "",
+    title: "Arrays in javascript can be used to store ________.",
+    choice1: "numbers & strings",
+    choice2: "other arrarys",
+    choice3: "booleans",
+    choice4: "all of the above",
+    answer: 4
+  },
+  {
+    title: "What is the correct syntax for adding comments in JavaScript?",
+    choice1: "<!–This is a comment",
+    choice2: "//This is a comment",
+    choice3: "–This is a comment",
+    choice4: "**This is a comment**",
+    answer: 2
+  },
+  {
+    title: "What is the JavaScript syntax for printing values in Console?",
+    choice1: "print(5)",
+    choice2: "console.log(5);",
+    choice3: "console.print(5);",
+    choice4: "print.console(5);",
+    answer: 2
   }
 ];
 
--- script.js
+++ script.js
@@ -1,54 +1,14 @@
-var questions = [ 
-  {
-    title: "Commonly used data types DO NOT include:",
-    choice1: "strings",
-    choice2: "booleans",
-    choice3: "alerts",
-    choice4: "numbers",
-    answer: 3
-  },
-  {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choice1: "quotes",
-    choice2: "curly brackets",
-    choice3: "parenthese",
-    choice4: "square brackets",
-    answer: 3
-  },
-  {
-    title: "Arrays in javascript can be used to store ________.",
-    choice1: "numbers & strings",
-    choice2: "other arrarys",
-    choice3: "booleans",
-    choice4: "all of the above",
-    answer: 4
-  },
-  {
-    title: "String values must be enclosed within ______ when being assigned variables.",
-    choice1: "commas",
-    choice2: "curly brackets",
-    choice3: "quotes",
-    choice4: "parentheses",
-    answer: 2
-  },
-  {
-    title: "A very useful tool to use during developement and debugging for printing content to the debugger is:",
-    choice1: "Javascript",
-    choice2: "terminal/bash",
-    choice3: "for loops",
-    choice4: "console log",
-    answer: 3
-  }
-];
 
 var question = document.getElementById("question");
 var scoreDiv = document.getElementById("score");
+var highScore = document.getElementById("highScore");
 var timer = document.getElementById("timer");
 var title = document.getElementById("title");
 var choice1 = document.getElementById("choice1");
 var choice2 = document.getElementById("choice2");
 var choice3 = document.getElementById("choice3");
 var choice4 = document.getElementById("choice4");
+var highScoreName = document.getElementById("name");
 
 choice1.onclick = () => onAnswer(1);
 choice2.onclick = () => onAnswer(2);
@@ -119,6 +79,9 @@ function onAnswer(selectedChoice) {
     score++;
     scoreDiv.innerText = "Score: " + score;
   }
+  else {
+    time = time - 5;
+  }
   questionCounter++;
   getNewQuestion();
 }
@@ -127,4 +90,36 @@ function endGame() {
   question.className = "hidden";
   results.className = "";
   clearInterval(timerSetIntervalId);
+  highScores();
+}
+
+function highScores() {
+  var submitBtn = document.getElementById("submit");
+  submitBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    var name = highScoreName.value; 
+    localStorage.setItem("score", score);
+    localStorage.setItem("name", name);
+  })
+
+  var initial = localStorage.getItem("name");
+  var newScore = localStorage.getItem("score");
+  highScore.innerHTML = "High Scores: " + initial + " " + newScore;
+
+
+  var scores = [
+    { name: "gary", score: 10 },
+    { name: "", score: 10 },
+    { name: "", score: 10 }
+  ]
+
+  var findPlayerScore = function(){
+    // read all scores from localstorage
+    // loop through scores; if name = currentplayer name, then make the score the currenr score
+  }
+
+  // new player enters name
+  var player = { name: "name", score: 0 };
+
+
 }
